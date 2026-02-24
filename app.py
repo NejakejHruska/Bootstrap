b5033c1f7d7151bd5ceb8536f53585728e824beb
NejakejHruska <24305@student.vitkovickastredni.cz>
2026-02-24 11:35:23 +0100

    flexbox

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.htm')
 
+@app.route('/flexbox')
+def flexbox():
+    return render_template('flexbox.htm')
+
 if __name__ == '__main__':
     app.run(debug=True)
